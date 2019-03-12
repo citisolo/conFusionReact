88e07f9378297c18571fa4c2b5342d1de48a2958
kwadwo amankwa <citisolo@gmail.com>
2019-03-12 10:48:37 +0000

    Assignment 1

---
+++ src/components/DishdetailComponent.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+
+class DishDetail extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  renderDish(dish){
+    if(dish == null){
+      return (<div></div>);
+    }
+
+    return (
+      <div className="row">
+        <div className="col-12 col-md-5 m-1">
+          <Card>
+            <CardImg width="100%" src={dish.image} alt={dish.name}/>
+            <CardBody>
+              <CardTitle>{dish.name}</CardTitle>
+              <CardText>{dish.description}</CardText>
+            </CardBody>
+          </Card>
+        </div>
+        <div className="col-12 col-md-5 m-1">
+          {this.renderComments(dish.comments)}
+        </div>
+      </div>
+    )
+  }
+
+  renderComments(comments){
+    if(comments == null){
+      return (<div></div>);
+    }
+
+    let commentList = comments.map((comment) => {
+        return (
+          <div key={comment.id}>
+            <p>{comment.comment}</p>
+            <p>{comment.author}, {comment.date}</p>
+          </div>
+        )
+    });
+
+    return (
+      <div>
+        <h4> Comments </h4>
+        {commentList}
+      </div>
+    )
+  }
+
+  render(){
+
+    return (
+      <div>
+        {this.renderDish(this.props.dish)}
+      </div>
+    )
+  }
+}
+
+export default DishDetail;
