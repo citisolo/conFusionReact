0753123c376b0d8d5985fee4b0bae44485c7c099
kwadwo amankwa <citisolo@gmail.com>
2019-03-15 11:59:58 +0000

    Presentational and Container Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -12,6 +12,7 @@ class DishDetail extends Component {
     }
 
     return (
+    <div className="container">
       <div className="row">
         <div className="col-12 col-md-5 m-1">
           <Card>
@@ -26,6 +27,7 @@ class DishDetail extends Component {
           {this.renderComments(dish.comments)}
         </div>
       </div>
+    </div>
     )
   }
 
@@ -38,7 +40,7 @@ class DishDetail extends Component {
         return (
           <div key={comment.id}>
             <p>{comment.comment}</p>
-            <p>{comment.author}, {comment.date}</p>
+            <p>{comment.author}, {new Intl.DateTimeFormat('en-UK', {year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
           </div>
         )
     });
