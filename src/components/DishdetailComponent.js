660033da230b4435fa96f858b37d93997c10ce82
kwadwo amankwa <citisolo@gmail.com>
2019-03-18 11:05:09 +0000

    Functional Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,66 +1,65 @@
-import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import React from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
-class DishDetail extends Component {
-  constructor(props){
-    super(props);
+const RenderDish = ({dish}) => {
+  if(dish == null){
+    return (<div></div>);
   }
 
-  renderDish(dish){
-    if(dish == null){
-      return (<div></div>);
-    }
+  return (
+    <Card>
+      <CardImg width="100%" src={dish.image} alt={dish.name}/>
+      <CardBody>
+        <CardTitle>{dish.name}</CardTitle>
+        <CardText>{dish.description}</CardText>
+      </CardBody>
+    </Card>
+  )
+}
 
-    return (
+const RenderComments = ({comments}) => {
+
+  if(comments == null){
+    return (<div></div>);
+  }
+
+  let commentList = comments.map((comment) => {
+      return (
+        <div key={comment.id}>
+          <p>{comment.comment}</p>
+          <p>{comment.author}, {new Intl.DateTimeFormat('en-UK', {year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+        </div>
+      )
+  });
+
+  return (
+    <div>
+      <h4> Comments </h4>
+      {commentList}
+    </div>
+  )
+}
+
+const DishDetail = (props) => {
+  let dish = props.dish;
+  let comments = null;
+  if (dish != null){
+    comments = dish.comments;
+  }
+
+  return (
     <div className="container">
       <div className="row">
         <div className="col-12 col-md-5 m-1">
-          <Card>
-            <CardImg width="100%" src={dish.image} alt={dish.name}/>
-            <CardBody>
-              <CardTitle>{dish.name}</CardTitle>
-              <CardText>{dish.description}</CardText>
-            </CardBody>
-          </Card>
+          <RenderDish dish={dish}/>
         </div>
         <div className="col-12 col-md-5 m-1">
-          {this.renderComments(dish.comments)}
+          <RenderComments comments={comments}/>
         </div>
       </div>
     </div>
-    )
-  }
-
-  renderComments(comments){
-    if(comments == null){
-      return (<div></div>);
-    }
-
-    let commentList = comments.map((comment) => {
-        return (
-          <div key={comment.id}>
-            <p>{comment.comment}</p>
-            <p>{comment.author}, {new Intl.DateTimeFormat('en-UK', {year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
-          </div>
-        )
-    });
-
-    return (
-      <div>
-        <h4> Comments </h4>
-        {commentList}
-      </div>
-    )
-  }
-
-  render(){
-
-    return (
-      <div>
-        {this.renderDish(this.props.dish)}
-      </div>
-    )
-  }
+  )
 }
+
 
 export default DishDetail;
