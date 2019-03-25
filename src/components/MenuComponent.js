5fcc7ec2f254d31ae40eb51e6000c869343acfb3
Kwadwo Amankwa <citisolo@gmail.com>
2019-03-25 06:09:57 +0000

    Redux Thunk

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Loading } from './LoadingComponent';
 
 const RenderMenuItem = ({dish, onClick}) => {
+
+
   return(
     <Card>
       <Link to={`/menu/${dish.id}`}>
@@ -16,12 +19,36 @@ const RenderMenuItem = ({dish, onClick}) => {
 }
 
 const Menu = (props) => {
-  const menu = props.dishes.map((dish) => {
-     return(
-       <div key={dish.id} className="col-12 col-md-5 m-1">
-         <RenderMenuItem dish={dish} onClick={props.onClick}/>
-       </div>
-     )
+  const menu = props.dishes.dishes.map((dish) => {
+
+        if (props.dishes.isLoading) {
+            return(
+                <div className="container">
+                    <div className="row">
+                        <Loading />
+                    </div>
+                </div>
+            );
+        }
+        else if (props.dishes.errMess) {
+            return(
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12">
+                            <h4>{props.dishes.errMess}</h4>
+                        </div>
+                    </div>
+                </div>
+            );
+        }
+        else{
+          return(
+            <div key={dish.id} className="col-12 col-md-5 m-1">
+              <RenderMenuItem dish={dish} onClick={props.onClick}/>
+            </div>
+          )  
+        }
+
   });
 
   return(
