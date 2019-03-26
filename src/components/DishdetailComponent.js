6ac592ff3bd4318acf148036624e33a98d6f2be8
Kwadwo Amankwa <citisolo@gmail.com>
2019-03-26 05:54:49 +0000

    React Animation Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -4,6 +4,7 @@ import {  Card, CardImg, CardText, CardBody,
   BreadcrumbItem, Modal, ModalHeader, ModalBody, Form,
   FormGroup, Label, Input, Col, Row } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
@@ -125,13 +126,19 @@ const RenderDish = ({dish}) => {
   }
 
   return (
+    <FadeTransform
+        in
+        transformProps={{
+            exitTransform: 'scale(0.5) translateY(-50%)'
+        }}>
     <Card>
-      <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name}/>
-      <CardBody>
-        <CardTitle>{dish.name}</CardTitle>
-        <CardText>{dish.description}</CardText>
-      </CardBody>
+        <CardImg top src={baseUrl + dish.image} alt={dish.name} />
+        <CardBody>
+            <CardTitle>{dish.name}</CardTitle>
+            <CardText>{dish.description}</CardText>
+        </CardBody>
     </Card>
+    </FadeTransform>
   )
 }
 
@@ -153,7 +160,18 @@ const RenderComments = ({dishId, postComment, comments}) => {
   return (
     <div>
       <h4> Comments </h4>
-      {commentList}
+      <Stagger in>
+          {comments.map((comment) => {
+              return (
+                  <Fade in>
+                  <li key={comment.id}>
+                  <p>{comment.comment}</p>
+                  <p>-- {comment.author} , {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+                  </li>
+                  </Fade>
+              );
+          })}
+      </Stagger>
       <CommentForm dishId={dishId} postComment={postComment}/>
     </div>
   )
